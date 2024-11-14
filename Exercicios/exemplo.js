1b39a8fe5d0b8e4c4fed49dd43aa83c64dd68507
Wayber <wayber1@hotmail.com>
2024-11-14 16:21:21 -0300

    exercicios novos

---
+++ Exercicios/exemplo.js
@@ -0,0 +1,4 @@
+ <script>src="exemplo.js"
+
+ </script>
+
---
+++ Exercicios/exercicio8.js
@@ -0,0 +1,12 @@
+function quadrado(x){
+var n1 = Number(x)
+var resultado =  n1 ** 2
+return resultado
+
+}
+console.log(2);
+console.log(5);
+console.log(10);
+console.log(0.5);
+
+
