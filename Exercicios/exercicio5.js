2db4678418617c7fe1ff2df597d38a6197b5894d
Wayber <wayber1@hotmail.com>
2024-11-29 16:57:07 -0300

    texte questõa 1e2

--- Exercicios/exercicio5.js
+++ Exercicios/exercicio5.js
@@ -3,3 +3,6 @@ function separarTexto(texto, separador) {
     var resultado = texto.split(separador)
     return resultado
 }
+
+console.log( separarTexto("O rato roeu a roupa do rei de Roma", " ") )
+console.log( separarTexto( "Ana Maria,Paulo Coelho,Carla" , ",") )
