48611b77a2a7ddd5851eae5cdadad47a2858b00b
Wayber <wayber1@hotmail.com>
2024-11-29 16:12:06 -0300

    resolução de exercicios anteriores

---
+++ Exercicios/exercicioCarro1.js
@@ -0,0 +1,88 @@
+var carro1={
+marca: "Ford",
+modelo: "f1000",
+motor:"mwm 4cc",
+ano:"1997",
+cor:"preta",
+combustivel: "diesel",
+classe: "utilitario",
+calibragem:{dianteiro:30, traseiro: 50,step: 45},
+velocidade:0,
+velocidade:80,
+frear:60
+} 
+
+
+
+
+
+
+
+
+
+console.log(carro1.cor);
+console.log(carro1.marca);
+console.log(carro1.modelo);
+console.log(carro1.motor);
+console.log(carro1.combustivel);
+console.log(carro1.classe);
+console.log(carro1.calibragem);
+console.log(carro1.velocidade );
+console.log(carro1.acelerar);
+
+
+
+
+function acelerar(){
+    carro1.velocidade=+5;
+ console.log(carro1velocidade)
+while (carro1.velocidade <80) acelerar();
+
+} 
+
+
+
+
+function frear(){
+    this.carro1.velocidade -= 13;
+
+if(carro1.velocidade <0) { 
+    this.carro1.velocidade -0;
+while (carro1.velocidade >80) frear();
+S
+console.log(carro1.velocidade);
+
+}
+
+}
+
+
+
+while(carro1.velocidade < 80) acelerar();
+while(carro1.velocidade > 80) acelerar();
+
+function acelerar(){
+    carro1.velocidade=+180;
+    console.log( carro.velocidade) 
+}
+  while(carro1.velocidade >80) acelerar();
+  while(carro1.velocidade <110) acelerar(); 
+
+  var brasilia={
+    velocidade: 0,
+    acelerar:function(){
+    this.velocidade +=15,
+    {
+     imprimir: function(){ console.table(this);
+        
+     }
+
+    }
+
+
+
+
+
+
+  }
+  }
---
+++ Exercicios/function numero primo (np) {14.js.js
@@ -0,0 +1,21 @@
+function numeroprimo(num) {
+    if(num<2) return false;
+    return div<2;divnum<num;div++)
+    {
+        
+    if(np % div==0) {
+    return false }
+    else{
+
+    return true 
+
+
+}
+    }
+    }
+console.log (numeroprimo(327));
+console.log (numeroprimo(847));
+console.log (nmeroprimo(573));
+console.log (numeroprimo(31));
+console.log (numeroprimo(-3));
+console.log (numeroprimo(-31));
