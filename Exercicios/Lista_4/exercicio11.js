25492a57db404e2353b9db00d5d2889f7d184a0c
Wayber <wayber1@hotmail.com>
2024-12-11 13:49:40 -0300

    exerciciosLista_4

--- Exercicios/Lista_4/exercicio11.js
+++ Exercicios/Lista_4/exercicio11.js
@@ -1,13 +1,14 @@
+var senario1 = [1, 2, 4, 5, 7, 8, 10, 11]
 function calcularMedia(arr) {
-if (arr.length === 0) {
+if (arr.length == 0) {
     return 0;
 } 
 let soma = 0;
 for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
+    
     soma += arr[i];
 }
     return soma / arr.length;
 }
-console.log(calcularMedia([1, 2, 4, 5, 7, 8, 10, 11]));
+console.log(calcularMedia(senario1));
 console.log(calcularMedia([10, 70, 30, 50]));
