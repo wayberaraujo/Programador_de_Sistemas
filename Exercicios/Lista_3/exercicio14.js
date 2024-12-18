1a5c943452f96d0f85cfe43f92d952d5f9d7918e
wayber <wayber1@hotmail.com>
2024-12-18 14:48:12 -0300

    reorganização de pastas

---
+++ Exercicios/Lista_3/exercicio14.js
@@ -0,0 +1,8 @@
+import   { numeroprimo } from '../biblioteca.js';
+
+console.log (numeroprimo(327));
+console.log (numeroprimo(847));
+console.log (numeroprimo(573));
+console.log (numeroprimo(31));
+console.log (numeroprimo(-3));
+console.log (numeroprimo(-31)); 
--- Exercicios/biblioteca.js
+++ Exercicios/biblioteca.js
@@ -34,7 +34,8 @@ function calcularMedia(arr) {
 if (arr.length == 0) {
     return 0;
 } 
-let soma = 0;
+
+  let soma = 0; 
 for (let i = 0; i < arr.length; i++) {
     
     soma += arr[i];
