d31d0e8fd69ab784e8a1f319281fd5213316350a
Wayber <wayber1@hotmail.com>
2024-12-18 14:02:14 -0300

    WIP: exercício 15

--- Exercicios/Lista_4/exercicio15.js
+++ Exercicios/Lista_4/exercicio15.js
@@ -1,5 +1,6 @@
-function reverseString(str) {
+function reverseString() {
     // Passo 1. Use o método split() para retornar um novo array
     var splitString = str.split("hello"); // var splitString = "hello".split("");
     // ["h", "e", "l", "l", "o"]
 }
+console.log()
